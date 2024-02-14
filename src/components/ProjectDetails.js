caf6ffd88980b2284915be548cab014f7672bbe6
alihasanlitonislam <alihasan404439@gmail.com>
2024-02-14 21:02:02 +0600

    problem the code

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -5,13 +5,12 @@ import Slider from "react-slick";
 import Image from "next/image";
 import Link from "next/link";
 
-import { Rubik } from 'next/font/google'
- 
-const rubik = Rubik({
-  weight: '400',
-  subsets: ['latin'],
-})
+import { Rubik } from "next/font/google";
 
+const rubik = Rubik({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 function HomePage() {
   const sectionRef = useRef(null);
@@ -39,11 +38,12 @@ function HomePage() {
       }
     );
     return () => {
-      {/* A return function for killing the animation on component unmount */ }
+      {
+        /* A return function for killing the animation on component unmount */
+      }
       pin.kill();
     };
   }, []);
-
 
   const settings = {
     dots: true,
@@ -51,83 +51,62 @@ function HomePage() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    
   };
 
   return (
+    <>
+      <section className="scroll-section-outer">
+        <div ref={triggerRef}>
+          <div ref={sectionRef} className="scroll-section-inner">
+            <div className="scroll-section">
+              <div className="flex gap-5">
+                <div className="w-[710px] h-[740px]">
+                  <Link href="/">
+                    <Image
+                      src={"/img/luxevil.jpg"}
+                      width={710}
+                      height={780}
+                      className="object-cover w-[710px] h-[100%]"
+                    />
+                  </Link>
 
-    <>       
-            <section className="scroll-section-outer">    
-                <div ref={triggerRef}>
-                    <div ref={sectionRef} className="scroll-section-inner">
-                       
+                  <div className="mt-4">
+                    <p
+                      style={{
+                        fontSize: "14px",
+                        fontWeight: 500,
+                        lineHeight: "15px",
+                        letterSpacing: "0em",
+                        textAlign: "left",
+                      }}
+                    >
+                      Luxe villa’s
+                    </p>
+                    <Link href="/">
+                      <p
+                        className="mt-3 underline"
+                        style={{
+                          position: "relative",
 
-                    <div className="scroll-section">
-                     
-
-                      <div className="flex gap-5">
-                          <div className="w-[710px] h-[740px]">
-                            <Link href='/'>
-                              <Image src={'/img/luxevil.jpg'} width={710} height={780} className="object-cover w-[710px] h-[100%]" />
-                            </Link>
-                            
-                            <div className="mt-4">
-                              <p
-                                style={{                            
-                                  fontSize: '14px',
-                                  fontWeight: 500,
-                                  lineHeight: '15px',
-                                  letterSpacing: '0em',
-                                  textAlign: 'left',
-                              
-
-                                }}    
-                              >Luxe villa’s</p>
-                              <Link href='/'>
-                                <p 
-
-                                className="mt-3 underline"
-
-                                  style={{
-                                    position: 'relative',
-
-                                    fontSize: '13px',
-                                    fontStyle: 'italic',
-                                    fontWeight: 300,
-                                    lineHeight: '15px',
-                                    letterSpacing: '-0.3499999940395355px',
-                                    textAlign: 'left',
-                                  
-
-                                  }}
-                                >
-                                  bekijk project                              
-                                </p>
-                              </Link>
-                              
-                            </div>
-                            
-                          </div>                        
-                      
-
-                       </div>
-
-                      
-
-                    </div>
-
-              
-                    </div>
+                          fontSize: "13px",
+                          fontStyle: "italic",
+                          fontWeight: 300,
+                          lineHeight: "15px",
+                          letterSpacing: "-0.3499999940395355px",
+                          textAlign: "left",
+                        }}
+                      >
+                        bekijk project
+                      </p>
+                    </Link>
+                  </div>
                 </div>
-            </section>    
-        
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-
-    
-
-    
-
-
   );
 }
 
