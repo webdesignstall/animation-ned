41d16d47e17fd303452435f976da3cd45acd935c
Jony Ahmed <sazzadrahman@Sazzads-Mac-mini.local>
2024-03-06 21:01:38 +0600

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
+        protocol: 'https',
         hostname: process.env.NEXT_PUBLIC_DOMAIN,
         port: '',
         pathname: '/wp-content/**',
