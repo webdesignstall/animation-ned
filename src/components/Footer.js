67329d4cc0c659b51e26251a023df0da28acd6f8
developeralamgirbd <developer.alamgirbd@gmail.com>
2024-03-26 21:30:55 +0600

    [Done] fix issue no 31 and 33

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Footer({data}) {
   return (
-    <div className="bg-[#EEEEEF] border-t-2 border-[#c4c4c4] py-5">
-      <div className="container mx-auto text-center md:text-left md:flex justify-between items-center my-2 text-sm md:text-xl px-5 md:p-0">
+    <div className="bg-[#EEEEEF] lg:border-t-2 border-[#c4c4c4] px-16">
+      <div className="px-16 text-center md:text-left md:flex justify-between items-center my-2 text-sm md:text-xl md:p-0">
         <div>
          <h1>{
              data?.copyRight?.leftText
--- src/components/Navber.js
+++ src/components/Navber.js
@@ -7,11 +7,9 @@ import Link from "next/link";
 
 const Navber=({data, categories}) =>{
 
-  console.log('data', data)
-
   return (
     <div className="sticky top-0 left-0 bottom-0 bg-[#EEEEEF] border-b-2 border-[#c4c4c4] mb-5 z-50">
-      <div className="hidden container mx-auto md:flex justify-between items-center my-5">
+      <div className="hidden mx-auto px-16 md:flex justify-between items-center my-5">
         <div className="flex gap-14">
           <Link href="/">
             <h2 className="text-[19px] italic font-[300] leading-[21px]">
