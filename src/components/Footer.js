9572297956bf1d890e0a0702ff05adcbb4386cee
developeralamgirbd <developer.alamgirbdg@gmail.com>
2024-05-08 20:15:08 +0600

    [Done] fix project cat page issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer({data}) {
   return (
 
 
-      <>
+      <div className='lg:border-t-[2px] lg:border-[#00000026]'>
 
          {/* <div className="bg-[#EEEEEF] lg:border-t-2 border-[#c4c4c4]">
               <div
@@ -21,7 +21,7 @@ export default function Footer({data}) {
                   </div>
               </div>
           </div>*/}
-          <div className='flex justify-center md:justify-between items-center py-3 lg:border-t-[2px] lg:border-[#00000026] md:mx-auto md:px-16'>
+          <div className='flex justify-center md:justify-between items-center py-3 md:mx-auto md:px-16'>
               <p></p>
               <p style={{fontWeight: 500, fontSize: 16}} className=''>
                   {data?.copyRight?.rightText} <a target={'_blank'} className='underline' style={{fontWeight: "bolder"}}
@@ -30,7 +30,7 @@ export default function Footer({data}) {
           </div>
 
 
-      </>
+      </div>
   )
       ;
 }
